8ab48bb59fa956599bafe4c3d6f15a9a31c1cadf
Pedro Montes <pedrosolbm@gmail.com>
2019-11-07 17:21:04 -0300

    postcontrol added

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,7 +1,27 @@
 const sequelize = require ('sequelize');
-
 const Post = require('../models/Post')
 
 module.exports = {
-    
-}
+    async index(req, res){
+        const post = await Post.findAll();
+        return res.json(post)
+    },
+    async search(req, res){
+        const post = await Post.findByPk(req.params.id);
+        return res.json(post);
+    },
+    async create(req, res){
+        const post = await Post.create(req.body);
+        return res.json(post);
+    },
+    async update(req, res){
+        const post = await Post.findByPk(req.params.id);
+        await post.update(req.body);
+        return res.json(post);
+    },
+    async destroy(req, res){
+        const post = await Post.findByPk(req.params.id);
+        await post.destroy();
+        return res.send();
+    }
+};
--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const UserController = require('./controllers/UserController');
-// const PostController = require('./controllers/PostController');
+const PostController = require('./controllers/PostController');
 
 const router = express.Router();
 
 router.get('/list', (req, res) => { return res.send("/list LISTAAAAA"); });
 
 router.get('/list/users', UserController.index);
-// router.get('/list/posts', PostController.index);
+router.get('/list/posts', PostController.index);
 
 router.get('/list/users/:id', UserController.search);
-// router.get('/list/posts/:id', PostController.search);
+router.get('/list/posts/:id', PostController.search);
 
 router.post('/register/users', UserController.create);
-// router.post('/register/posts', PostController.create);
+router.post('/register/posts', PostController.create);
 
 router.put('/update/users/:id', UserController.update);
-// router.put('/update/posts/:id', PostController.update);
+router.put('/update/posts/:id', PostController.update);
 
 router.delete('/delete/users/:id', UserController.destroy);
-// router.delete('/delete/posts/:id', PostsController.delete);
+router.delete('/delete/posts/:id', PostController.destroy);
 
 module.exports = router;
